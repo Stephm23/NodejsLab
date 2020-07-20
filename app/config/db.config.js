ef0a062e4fa67da12c5f2a7133121ac0115cd2d5
Stephm23 <Stephanie.mboya@strathmore.edu>
2020-07-20 12:58:23 +0300

    Initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "blp0mb2yzq8h1v2uui2d-mysql.services.clever-cloud.com",
+  USER: "uoynvbzluzmkbuy1",
+  PASSWORD: "F6aV9oEm8TbMnW3JMXCC",
+  DB: "blp0mb2yzq8h1v2uui2d"
+};
